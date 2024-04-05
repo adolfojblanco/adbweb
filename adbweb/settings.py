c2b4ba4cb2709de0e1f3e5df128dde564b6379b9
Adolfo Blanco <ajb156@outlook.com>
2024-04-05 19:39:20 +0200

    update

--- adbweb/settings.py
+++ adbweb/settings.py
@@ -4,6 +4,10 @@ Django settings for adbweb project.
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
